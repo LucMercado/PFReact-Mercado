c28f023bcda7c9809b51093d1fa398f8db94ce18
Lucas Mercado <lucasmmercado99@gmail.com>
2023-08-31 20:56:05 -0300

    Correcion ruta img

--- src/services.js
+++ src/services.js
@@ -1,11 +1,11 @@
 const products = [
-    {id: "1", name: "Ladrillo", price: 100, category: "ladrillos", image: "public/img/ladrillo.jpg"},
-    {id: "2", name: "Ladrillo Block", price: 130, category: "ladrillos", image: "public/img/ladrillo-block.jpg"},
-    {id: "3", name: "Bolson Piedra", price: 10000, category: "bolsones", image: "public/img/bolson-piedra.jpg"},
-    {id: "4", name: "Bolson Arena", price: 8000, category: "bolsones", image: "public/img/arena.jpg"},
-    {id: "5", name: "Ladrillo Hueco", price: 120, category: "ladrillos", image: "public/img/ladrillo-hueco.jpg"},
-    {id: "6", name: "Cemento", price: 700, category: "cementos", image: "public/img//bolsa-cemento.png"},
-    {id: "7", name: "Cemento Hydralit", price: 900, category: "cementos", image: "public/img/hidralit.png"},
+    {id: "1", name: "Ladrillo", price: 100, category: "ladrillos", image: "img/ladrillo.jpg"},
+    {id: "2", name: "Ladrillo Block", price: 130, category: "ladrillos", image: "img/ladrillo-block.jpg"},
+    {id: "3", name: "Bolson Piedra", price: 10000, category: "bolsones", image: "img/bolson-piedra.jpg"},
+    {id: "4", name: "Bolson Arena", price: 8000, category: "bolsones", image: "img/arena.jpg"},
+    {id: "5", name: "Ladrillo Hueco", price: 120, category: "ladrillos", image: "img/ladrillo-hueco.jpg"},
+    {id: "6", name: "Cemento", price: 700, category: "cementos", image: "img//bolsa-cemento.png"},
+    {id: "7", name: "Cemento Hydralit", price: 900, category: "cementos", image: "img/hidralit.png"},
 ]
 
 export const getProduct = (id) => {
